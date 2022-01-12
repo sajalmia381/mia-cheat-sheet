b3189fbdfc5baf94b812096cc1a02423c29b6391
Md Sajal Mia <sajalmia381@gmail.com>
2022-01-12 11:54:10 +0600

    update

--- docker/express-api/index.js
+++ docker/express-api/index.js
@@ -1,6 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
-const port = 3000
+const port = 8000
+
+const connectMongoRetryLoop = () => {
+  mongoose.connect('mongodb://root:hello123@mongo:27017/express-api-db?authSource=admin')
+  .then(() => console.log("mongodb: mongo database connected"))
+  .catch((e) => {
+    console.log("mongodb: mongo connection failed")
+    console.log('failed', e)
+    setTimeout(() => {connectMongoRetryLoop}, 10000)
+  })
+}
+connectMongoRetryLoop();
 
 app.get('/', (req, res) => {
   res.json({
